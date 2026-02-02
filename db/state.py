38010b240fe9854303995e54994b6193eb2142b7
Jakob Wandel <Sonnentau.koenig@gmail.com>
2026-02-02 14:32:32 +0100

    fuuu

--- db/state.py
+++ db/state.py
@@ -5,7 +5,7 @@ from pathlib import Path
 DB_FILE = Path(__file__).parent / "state.db"
 
 def get_connection():
-    conn = sqlite3.connect(DB_PATH)
+    conn = sqlite3.connect(DB_FILE)
     conn.row_factory = sqlite3.Row
     return conn
 
